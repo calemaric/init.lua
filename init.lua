a8ed5e5fc47a3e695cbe62e9446014a70f46baad
Marko Maric <marko.maric@servicenow.com>
2024-05-29 16:52:33 +0200

    Add keybinds for folder

--- init.lua
+++ init.lua
@@ -213,7 +213,8 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<space>fb", function()
+			vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+			vim.keymap.set("n", "<space>fB", function()
 				require("telescope").extensions.file_browser.file_browser()
 			end)
 
