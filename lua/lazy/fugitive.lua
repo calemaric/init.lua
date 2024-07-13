bc3f7f0f4b2638f5a61ec9cc10db4e332732cfad
calemaric <calemaric@gmail.com>
2024-07-13 16:55:44 +0200

    Change group

--- lua/lazy/fugitive.lua
+++ lua/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-		local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+		local calemaric_Fugitive = vim.api.nvim_create_augroup("calemaric_Fugitive", {})
 
 		local autocmd = vim.api.nvim_create_autocmd
 		autocmd("BufWinEnter", {
-			group = ThePrimeagen_Fugitive,
+			group = calemaric_Fugitive,
 			pattern = "*",
 			callback = function()
 				if vim.bo.ft ~= "fugitive" then
@@ -18,11 +18,6 @@ return {
 				local opts = { buffer = bufnr, remap = false }
 				vim.keymap.set("n", "<leader>p", function()
 					vim.cmd.Git("push")
-				end, opts)
-
-				-- rebase always
-				vim.keymap.set("n", "<leader>P", function()
-					vim.cmd.Git({ "pull", "--rebase" })
 				end, opts)
 
 				-- NOTE: It allows me to easily set the branch i am pushing and any tracking
