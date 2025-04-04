81b43d88d485405946179918666379a1d6740ea2
calemaric <calemaric@gmail.com>
2025-04-04 21:02:17 +0200

    Add commands

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -3,7 +3,12 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local gitblame = require("gitblame")
-		vim.keymap.set("n", "<leader>gc", gitblame.copy_commit_url_to_clipboard, { desc = "[G]it [C]ommit URL" })
-		vim.keymap.set("n", "<leader>go", gitblame.open_commit_url, { desc = "[G]it [O]pen Commit URL" })
+		vim.keymap.set(
+			"n",
+			"<leader>gbc",
+			gitblame.copy_commit_url_to_clipboard,
+			{ desc = "[G]it [B]lame [C]ommit URL" }
+		)
+		vim.keymap.set("n", "<leader>gbo", gitblame.open_commit_url, { desc = "[G]it [B]lame [O]pen Commit URL" })
 	end,
 }
