05a91602a6c96eab3053a2dc6df2ddb34cb1e717
Marko Maric <marko.maric@servicenow.com>
2025-04-06 10:10:33 +0200

    Add gitlinker

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,21 +1,10 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = {
+		"ruifm/gitlinker.nvim",
+	},
 	config = function()
-		local get_gbrowse = function()
-			local cursor_position = vim.api.nvim_win_get_cursor(0)[1]
-			return cursor_position .. "GBrowse"
-		end
-		local get_open_command = function()
-			vim.cmd(get_gbrowse())
-		end
-
-		local get_copy_command = function()
-			vim.cmd(get_gbrowse() .. "!")
-		end
-
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
-		vim.keymap.set("n", "<leader>go", get_open_command, { desc = "[O]pen Line" })
-		vim.keymap.set("n", "<leader>gc", get_copy_command, { desc = "[C]opy Line" })
 
 		local autocmd = vim.api.nvim_create_autocmd
 		autocmd("BufWinEnter", {
